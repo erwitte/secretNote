dc88a9bfa4d14c4eff28b141739dd158f17f1c69
Erik Witte <witte.erik2@gmail.com>
2026-01-07 16:50:08 +0100

    copying by button

--- frontend/src/link.jsx
+++ frontend/src/link.jsx
@@ -1,8 +1,12 @@
 function Link(){
+    const handleCopy = async() => {
+        await navigator.clipboard.writeText("copied");
+    }
     return(
         <>
         <div>
             <input type="text" value="Preset Text" readOnly />
+            <button onClick={handleCopy}></button>
         </div>
         </>
     )
